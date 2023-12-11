2877eb03f7d7e8a70be443ce85a16667479dae79
--global <--global>
2023-12-11 17:48:59 +0200

    Revised simpler version of the code for error handling

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_smorest import Api
+from sqlalchemy.orm import sessionmaker
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_smorest import Api
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
+
+app = Flask(__name__)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bikedb.sqlite'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db = SQLAlchemy(app)
+# Session = sessionmaker(bind=db.engine)
+
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "Stores REST API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bikedb.sqlite'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
+api = Api(app)
+
+api.register_blueprint(ItemBlueprint)
+api.register_blueprint(StoreBlueprint)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ schemas/models/models.py
@@ -0,0 +1,99 @@
+import uuid
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+#from app import db, Session
+
+Base = declarative_base()
+
+
+def generate_uuid():
+    return str(uuid.uuid4())
+
+# pyörät
+class Bike(Base):
+    __tablename__ = 'bike'
+
+    id = Column(String, primary_key=True, default=generate_uuid)
+    brand = Column(String, nullable=False)
+    size = Column(String, nullable=False)
+    user = Column(String, nullable=False) #male/female
+    bike_status = Column(String, nullable=False, default='available')
+
+    def dict(self):
+        return {
+            'id': self.id,
+            'brand': self.brand,
+            'size': self.size,
+            'user': self.user,
+            'bike_status': self.bike_status
+        }
+
+
+#asiakkaat
+class Customer(Base):
+    __tablename__ = 'customer'
+
+    id = Column(String, primary_key=True, default=generate_uuid)
+    name = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    address = Column(String, nullable=False)
+
+    def dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'phone': self.phone,
+            'email': self.email,
+            'address': self.address
+        }
+
+
+#vuokraus
+class Rental(Base):
+    __tablename__ = 'rental'
+
+    id = Column(String, primary_key=True, default=generate_uuid)
+    bike_id = Column(String, ForeignKey('bike.id'))
+    customer_id = Column(String, ForeignKey('customer.id'))
+    status = Column(String, nullable=False, default='created')
+    created = Column(DateTime, default=datetime.utcnow)
+    delivery_id = Column(String)
+
+    def dict(self):
+        return {
+            'id': self.id,
+            'bike_id': self.bike_id,
+            'customer_id': self.customer_id,
+            'status': self.status,
+            'created': self.created,
+            'delivery_id': self.delivery_id,
+        }
+
+#huolto
+class Maintenance(Base):
+    __tablename__ = 'maintenance'
+
+    id = Column(String, primary_key=True, default=generate_uuid)
+    bike_id = Column(String, ForeignKey('bike.id'))
+    status = Column(String, nullable=False, default='created')
+    created = Column(DateTime, default=datetime.utcnow)
+    
+
+    def dict(self):
+        return {
+            'id': self.id,
+            'bike_id': self.bike_id,
+            'status': self.status,
+            'created': self.created,
+        }
+
+    
+
+
+
+
+
